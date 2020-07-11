e7fb1cab50bf398357b9d8f75a3528a8a99b0dc1
jotasato <arukanaika.aruka.jj@gmail.com>
2020-07-11 23:08:17 +0900

    乱数取得してタップ処理で表示の変更実装

--- app/src/main/java/net/sajo/diceroller/MainActivity.kt
+++ app/src/main/java/net/sajo/diceroller/MainActivity.kt
@@ -1,11 +1,28 @@
 package net.sajo.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+
     }
 }
